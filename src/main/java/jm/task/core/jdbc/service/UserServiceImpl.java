18a3389f46e0b3a1b85e350b01f0ee5fcd3ea74c
shish81 <74242503+shish81@users.noreply.github.com>
2023-08-11 20:02:41 +0300

    Update UserServiceImpl.java
    удаление объекта DaoHibernet

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,10 +7,7 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private final UserDao dao =
-//        new UserDaoJDBCImpl();
-
-        new UserDaoHibernateImpl();
+    private final UserDao dao =   new UserDaoJDBCImpl();
 
 
     public void createUsersTable() {
